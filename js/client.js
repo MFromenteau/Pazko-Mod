6fc4a75508b414d748a1c5e1eb872e4f56de1c41
Marvin Fromenteau <marvin-dbz@hotmail.fr>
2017-11-09 14:01:21 +0100

    reindent all

--- js/client.js
+++ js/client.js
@@ -5,12 +5,12 @@ Client.socket = io.connect();
 
 //*********  EMITERS *****************
 Client.sendTest = function(){
-    console.log("test sent");
-    Client.socket.emit('test');
+  console.log("test sent");
+  Client.socket.emit('test');
 };
 
 Client.sendNewPlayer = function(){
-    Client.socket.emit('newplayer');
+  Client.socket.emit('newplayer');
 };
 
 Client.sendClick = function(x,y){
@@ -19,24 +19,24 @@ Client.sendClick = function(x,y){
 
 //************* RECEPTIONERS ***************
 Client.socket.on('newplayer',function(data){
-    Game.addNewPlayer(data.id,data.x,data.y);
+  Game.addNewPlayer(data.id,data.x,data.y);
 });
 
 Client.socket.on('allplayers',function(data){
-    for(var i = 0; i < data.length; i++){
-        Game.addNewPlayer(data[i].id,data[i].x,data[i].y);
-    }
+  for(var i = 0; i < data.length; i++){
+    Game.addNewPlayer(data[i].id,data[i].x,data[i].y);
+  }
 
-    /* 'move' and 'remove' could be written outside this function but that
-     *  means it would be possible to remove a player from a list
-     *  not yet received. SO always create your sender and receiver when they
-     * need to be created, even it's inside another async events
-     */
-    Client.socket.on('move',function(data){
-        Game.movePlayer(data.id,data.x,data.y);
-    });
+  /* 'move' and 'remove' could be written outside this function but that
+  *  means it would be possible to remove a player from a list
+  *  not yet received. SO always create your sender and receiver when they
+  * need to be created, even it's inside another async events
+  */
+  Client.socket.on('move',function(data){
+    Game.movePlayer(data.id,data.x,data.y);
+  });
 
-    Client.socket.on('remove',function(id){
-        Game.removePlayer(id);
-    });
+  Client.socket.on('remove',function(id){
+    Game.removePlayer(id);
+  });
 });
--- js/game.js
+++ js/game.js
@@ -2,67 +2,66 @@ var Game = {};
 
 // ************** INITIALISATION ****************
 Game.init = function(){
-    game.stage.disableVisibilityChange = true;
+  game.stage.disableVisibilityChange = true;
 };
 
 //preload all ressources, you can load it on the fly to but what can be preloaded should be here
 Game.preload = function() {
-    game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
-    game.load.image('player','assets/sprites/player.png');
+  game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
+  game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
+  game.load.image('player','assets/sprites/player.png');
 };
 
 
 Game.create = function(){
-    Game.playerMap = {};
-    var map = game.add.tilemap('map');
-    map.addTilesetImage('tilesheet', 'tileset'); // tilesheet is the key of the tileset in map's JSON file
-    var layer;
-    for(var i = 0; i < map.layers.length; i++) {
-        layer = map.createLayer(i);
-    }
+  Game.playerMap = {};
+  var map = game.add.tilemap('map');
+  map.addTilesetImage('tilesheet', 'tileset'); // tilesheet is the key of the tileset in map's JSON file
+  var layer;
+  for(var i = 0; i < map.layers.length; i++) {
+    layer = map.createLayer(i);
+  }
 
-    layer.inputEnabled = true; // Allows clicking on the map ; it's enough to do it on the last layer
+  layer.inputEnabled = true; // Allows clicking on the map ; it's enough to do it on the last layer
 
-    //from now on : on ANY input, send coordinate
-    layer.events.onInputUp.add(Game.getCoordinates, this);
+  //from now on : on ANY input, send coordinate
+  layer.events.onInputUp.add(Game.getCoordinates, this);
 
-
-    //function to notify of our presence on the Server
-    /* NOTE : We dosen't exist in the game yet, we will instanciate our presence
-    *  just before, or after everybody else. Cause the process is :
-    *  - Me : I connected ! (emit('newplayer'))
-    *  - Se : Ok here is your getCoordinates
-    *  - Me : Got it ! displaying all that shit
-    *  - Se : And here is all the players
-    *  - Me : Got it ! displaying all that shit
-    */
-    Client.sendNewPlayer();
+  //function to notify of our presence on the Server
+  /* NOTE : We dosen't exist in the game yet, we will instanciate our presence
+  *  just before, or after everybody else. Cause the process is :
+  *  - Me : I connected ! (emit('newplayer'))
+  *  - Se : Ok here is your getCoordinates
+  *  - Me : Got it ! displaying all that shit
+  *  - Se : And here is all the players
+  *  - Me : Got it ! displaying all that shit
+  */
+  Client.sendNewPlayer();
 };
 
 
 // ************ GAME FUNCTIONS **********************
 Game.getCoordinates = function(layer,pointer){
-    Client.sendClick(pointer.worldX,pointer.worldY);
+  Client.sendClick(pointer.worldX,pointer.worldY);
 };
 
 Game.addNewPlayer = function(id,x,y){
-    Game.playerMap[id] = game.add.sprite(x,y,'player');
+  Game.playerMap[id] = game.add.sprite(x,y,'player');
 };
 
 Game.movePlayer = function(id,x,y){
-    var player = Game.playerMap[id];
-    var distance = Phaser.Math.distance(player.x,player.y,x,y);
+  var player = Game.playerMap[id];
+  var distance = Phaser.Math.distance(player.x,player.y,x,y);
 
-    //Tween is to initaite a change over a period of time,
-    // like a character moving or a sprite fading
-    var tween = game.add.tween(player);
-    var duration = distance*10;
-    tween.to({x:x,y:y}, duration);
-    tween.start();
+  //Tween is to initaite a change over a period of time,
+  // like a character moving or a sprite fading
+  var tween = game.add.tween(player);
+  var duration = distance*10;
+  tween.to({x:x,y:y}, duration);
+  tween.start();
 };
 
 Game.removePlayer = function(id){
-    Game.playerMap[id].destroy();
-    delete Game.playerMap[id];
+  Game.playerMap[id].destroy();
+  delete Game.playerMap[id];
 };
